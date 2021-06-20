aed1aa5024c72b6c0dc01a290d56bdf26118f6d1
liufeng <liufeng.lf@sunyur.com>
2021-06-20 18:36:36 +0800

    做了几个简单的和正则表达式难度大的

--- src/main/java/com/study/leetcode/HotProblems.java
+++ src/main/java/com/study/leetcode/HotProblems.java
@@ -1,12 +1,167 @@
 package com.study.leetcode;
 
+import java.util.*;
+
 /**
  * 热题
  */
 public class HotProblems {
 
+    // -------正则表达式匹配 start >>--------
 
-    public String longestPalindrome(String s) {
-        return s;
+    public boolean isMatch(String s, String p) {
+        int m = s.length();
+        int n = p.length();
+
+        boolean[][] f = new boolean[m + 1][n + 1];
+        f[0][0] = true;
+
+        for (int i = 0; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (p.charAt(j - 1) == '*') {
+                    f[i][j] = f[i][j - 2];
+                    if (matches(s, p, i, j - 1)) {
+                        f[i][j] = f[i][j] || f[i - 1][j];
+                    }
+                } else {
+                    if (matches(s, p, i, j)) {
+                        f[i][j] = f[i - 1][j - 1];
+                    }
+                }
+            }
+        }
+        return f[m][n];
     }
+
+    private boolean matches(String s, String p, int i, int j) {
+        if (i == 0) {
+            return false;
+        }
+        if (p.charAt(j - 1) == '.') {
+            return true;
+        }
+        return s.charAt(i - 1) == p.charAt(j - 1);
+    }
+
+    // -------正则表达式匹配 << end --------
+
+    // -------盛最多水的容器 start >>--------
+
+    /**
+     * 双指针法
+     *
+     * @param height arr of height
+     * @return result
+     */
+    public int maxArea(int[] height) {
+        if (null == height || height.length == 0) {
+            return 0;
+        }
+        int length = height.length, i = 0, j = length - 1;
+        int result = 0;
+        while (i < j) {
+            int min = Math.min(height[i], height[j]);
+            result = Math.max(result, (j - i) * min);
+            if (height[i] < height[j]) {
+                i++;
+            } else {
+                j--;
+            }
+        }
+        return result;
+    }
+
+    // -------盛最多水的容器 << end --------
+
+    // -------三数之和 start >>--------
+
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (null == nums || nums.length < 3) {
+            return result;
+        }
+
+        Arrays.sort(nums);
+        if (nums[0] > 0 || nums[nums.length - 1] < 0) {
+            return result;
+        }
+        int len = nums.length;
+        for (int i = 0; i < len; i++) {
+            if (nums[i] > 0) {
+                return result;
+            }
+
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            int curr = nums[i];
+            int L = i + 1, R = len - 1;
+            while (L < R) {
+                int temp = curr + nums[L] + nums[R];
+                if (temp == 0) {
+                    List<Integer> list = new ArrayList<>();
+                    list.add(curr);
+                    list.add(nums[L]);
+                    list.add(nums[R]);
+                    result.add(list);
+                    while (L < R && nums[L + 1] == nums[L]) L++;
+                    while (L < R && nums[R - 1] == nums[R]) R--;
+                    L++;
+                    R--;
+                } else if (temp < 0) {
+                    L++;
+                } else {
+                    R--;
+                }
+            }
+        }
+        return result;
+    }
+
+    // -------三数之和 << end --------
+
+    // -------电话号码的字母组合 start >>--------
+
+    public List<String> letterCombinations(String digits) {
+        List<String> combinations = new ArrayList<>();
+        if (digits == null || digits.length() == 0) {
+            return combinations;
+        }
+
+        HashMap<Character, String[]> map = new HashMap<Character, String[]>() {{
+            put('2', new String[] {"a", "b", "c"});
+            put('3', new String[]{"d", "e", "f"});
+            put('4', new String[]{"g", "h", "i"});
+            put('5', new String[]{"j", "k", "l"});
+            put('6', new String[]{"m", "n", "o"});
+            put('7', new String[]{"p", "q", "r", "s"});
+            put('8', new String[]{"t", "u", "v"});
+            put('9', new String[]{"w", "x", "y", "z"});
+        }};
+
+        Queue<String> queue = new LinkedList<>();
+        for (int i = 0; i < digits.length(); i++) {
+            queueLetterCombinations(queue, map.get(digits.charAt(i)));
+        }
+
+        combinations.addAll(queue);
+        return combinations;
+    }
+
+    private void queueLetterCombinations(Queue<String> queue, String[] letters) {
+        if (queue.size() == 0) {
+            queue.addAll(Arrays.asList(letters));
+        } else {
+            // 记录本次需要进行出队列组合的元素数量
+            int queueLength = queue.size();
+            for (int i = 0; i < queueLength; i++) {
+                String s = queue.poll();
+                for (String letter : letters) {
+                    queue.add(s + letter);
+                }
+            }
+        }
+    }
+
+    // -------电话号码的字母组合 << end --------
 }
--- src/test/java/com/study/leetcode/HotProblemTests.java
+++ src/test/java/com/study/leetcode/HotProblemTests.java
@@ -1,7 +1,12 @@
 package com.study.leetcode;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
 
 public class HotProblemTests {
 
@@ -13,7 +18,32 @@ public class HotProblemTests {
     }
 
     @Test
-    public void testLongestPalindrome() {
+    public void testRegexIsMatch() {
+        boolean result = problem.isMatch("aab", "c*a*b");
+        System.out.println(result);
+        System.out.println(problem.isMatch("mississippi", "mis*is*p*."));
+        System.out.println(problem.isMatch("ab", ".*"));
+    }
 
+    @Test
+    public void testMaxArea() {
+        int[] arr = {1,8,6,2,5,4,8,3,7};
+        assertEquals(problem.maxArea(arr), 49);
+        int[] arr2 = {1,1};
+        assertEquals(problem.maxArea(arr2), 1);
+        int[] arr3 = {4,3,2,1,4};
+        assertEquals(problem.maxArea(arr3), 16);
+    }
+
+    @Test
+    public void testThreeSum() {
+        int[] arr = {-1,0,1,2,-1,-4};
+        List<List<Integer>> result = problem.threeSum(arr);
+        System.out.println(result);
+    }
+
+    @Test
+    public void testLetterCombinations() {
+        System.out.println(problem.letterCombinations("23"));
     }
 }
