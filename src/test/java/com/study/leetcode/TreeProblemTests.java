ac0c16579e62c25c268040d65c0529e0d1b47cea
liufeng <liufeng.lf@sunyur.com>
2021-12-27 23:06:21 +0800

    BST 树的一些简单题目

--- src/test/java/com/study/leetcode/TreeProblemTests.java
+++ src/test/java/com/study/leetcode/TreeProblemTests.java
@@ -2,8 +2,8 @@ package com.study.leetcode;
 
 import org.junit.Before;
 import org.junit.Test;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNull;
+
+import static org.junit.Assert.*;
 
 public class TreeProblemTests {
 
@@ -16,21 +16,14 @@ public class TreeProblemTests {
 
     @Test
     public void testInvertTree() {
-        TreeNode node1 = new TreeNode(1);
-        TreeNode node3 = new TreeNode(3);
-        TreeNode node2 = new TreeNode(2, node1, node3);
-
-        TreeNode node6 = new TreeNode(6);
-        TreeNode node9 = new TreeNode(9);
-        TreeNode node7 = new TreeNode(7, node6, node9);
-
-        TreeNode node4 = new TreeNode(4, node2, node7);
-        TreeNode root = problem.invertTree(node4);
-        assertEquals(root, node4);
-        assertEquals(root.left, node7);
-        assertEquals(root.right, node2);
-        assertEquals(node2.left, node3);
-        assertEquals(node2.right, node1);
+        TreeNode node = buildCommonTree();
+        TreeNode left = node.left;
+        TreeNode root = problem.invertTree(node);
+        assertEquals(root.val, 4);
+        assertEquals(root.left.val, 7);
+        assertEquals(root.right.val, 2);
+        assertEquals(left.left.val, 3);
+        assertEquals(left.right.val, 1);
     }
 
     @Test
@@ -52,4 +45,193 @@ public class TreeProblemTests {
         assertEquals(node6.next, node7);
         assertNull(node7.next);
     }
+
+    @Test
+    public void testFlatten() {
+        TreeNode node = buildCommonTree();
+        problem.flatten(node);
+        assertNull(node.left);
+        assertEquals(node.right.val, 2);
+        assertNull(node.right.left);
+        assertEquals(node.right.right.val, 1);
+        assertEquals(node.right.right.right.val, 3);
+        assertEquals(node.right.right.right.right.val, 7);
+        assertEquals(node.right.right.right.right.right.val, 6);
+        assertEquals(node.right.right.right.right.right.right.val, 9);
+        assertNull(node.right.right.right.right.right.left);
+
+        node = buildCommonTree();
+        problem.flatten_v2(node);
+        assertNull(node.left);
+        assertEquals(node.right.val, 2);
+        assertNull(node.right.left);
+        assertEquals(node.right.right.val, 1);
+        assertEquals(node.right.right.right.val, 3);
+        assertEquals(node.right.right.right.right.val, 7);
+        assertEquals(node.right.right.right.right.right.val, 6);
+        assertEquals(node.right.right.right.right.right.right.val, 9);
+        assertNull(node.right.right.right.right.right.left);
+
+        node = buildCommonTree();
+        problem.flatten_v3(node);
+        assertNull(node.left);
+        assertEquals(node.right.val, 2);
+        assertNull(node.right.left);
+        assertEquals(node.right.right.val, 1);
+        assertEquals(node.right.right.right.val, 3);
+        assertEquals(node.right.right.right.right.val, 7);
+        assertEquals(node.right.right.right.right.right.val, 6);
+        assertEquals(node.right.right.right.right.right.right.val, 9);
+        assertNull(node.right.right.right.right.right.left);
+    }
+
+    @Test
+    public void testConstructMaximumBinaryTree() {
+        int[] nums = new int[]{3, 2, 1, 6, 0, 5};
+        TreeNode root = problem.constructMaximumBinaryTree(nums);
+        assertEquals(root.val, 6);
+        assertEquals(root.left.val, 3);
+        assertEquals(root.right.val, 5);
+        assertEquals(root.left.right.val, 2);
+        assertNull(root.left.left);
+        assertNull(root.left.right.left);
+        assertEquals(root.left.right.right.val, 1);
+        assertEquals(root.right.left.val, 0);
+        assertNull(root.right.right);
+    }
+
+    @Test
+    public void testBuildTree() {
+        int[] preOrder = new int[]{3, 9, 20, 15, 7};
+        int[] inOrder = new int[]{9, 3, 15, 20, 7};
+        TreeNode root = problem.buildTree(preOrder, inOrder);
+        assertEquals(root.val, 3);
+        assertEquals(root.left.val, 9);
+        assertEquals(root.right.val, 20);
+        assertNull(root.left.left);
+        assertNull(root.left.right);
+        assertEquals(root.right.left.val, 15);
+        assertEquals(root.right.right.val, 7);
+
+        TreeNode node = problem.buildTree_v2(preOrder, inOrder);
+        assertEquals(node.val, 3);
+        assertEquals(node.left.val, 9);
+        assertEquals(node.right.val, 20);
+        assertNull(node.left.left);
+        assertNull(node.left.right);
+        assertEquals(node.right.left.val, 15);
+        assertEquals(node.right.right.val, 7);
+    }
+
+    @Test
+    public void testBuildTreeByPost() {
+        int[] inorder = new int[]{9,3,15,20,7};
+        int[] postOrder = new int[]{9,15,7,20,3};
+        TreeNode root = problem.buildTreeByPost(inorder, postOrder);
+        assertEquals(root.val, 3);
+        assertEquals(root.left.val, 9);
+        assertNull(root.left.left);
+        assertNull(root.left.right);
+        assertEquals(root.right.val, 20);
+        assertEquals(root.right.left.val, 15);
+        assertEquals(root.right.right.val, 7);
+    }
+
+    @Test
+    public void testKthSmallest() {
+        TreeNode node2 = new TreeNode(2);
+        TreeNode node1 = new TreeNode(1, null, node2);
+        TreeNode node4 = new TreeNode(4);
+        TreeNode node3 = new TreeNode(3, node1, node4);
+
+        assertEquals(problem.kthSmallest(node3, 1), 1);
+        assertEquals(problem.kthSmallest(node3, 2), 2);
+        assertEquals(problem.kthSmallest(node3, 3), 3);
+        assertEquals(problem.kthSmallest(node3, 4), 4);
+    }
+
+    @Test
+    public void testConvertBST() {
+        TreeNode node8 = new TreeNode(8);
+        TreeNode node7 = new TreeNode(7, null, node8);
+        TreeNode node5 = new TreeNode(5);
+        TreeNode node6 = new TreeNode(6, node5, node7);
+
+        TreeNode node0 = new TreeNode(0);
+        TreeNode node3 = new TreeNode(3);
+        TreeNode node2 = new TreeNode(2, null, node3);
+        TreeNode node1 = new TreeNode(1, node0, node2);
+
+        TreeNode node4 = new TreeNode(4, node1, node6);
+        TreeNode root = problem.convertBST(node4);
+        assertEquals(root.val, 30);
+        assertEquals(root.left.val, 36);
+        assertEquals(root.left.left.val, 36);
+        assertEquals(root.left.right.val, 35);
+        assertEquals(root.left.right.right.val, 33);
+        assertEquals(root.right.val, 21);
+        assertEquals(root.right.left.val, 26);
+        assertEquals(root.right.right.val, 15);
+    }
+
+    @Test
+    public void testisValidBSTTree() {
+        TreeNode node1 = new TreeNode(1);
+        TreeNode node3 = new TreeNode(3);
+        TreeNode node2 = new TreeNode(2, node1, node3);
+
+        TreeNode node6 = new TreeNode(6);
+        TreeNode node4 = new TreeNode(4, node3, node6);
+        TreeNode node5 = new TreeNode(5, node1, node4);
+
+        assertTrue(problem.isValidBST(node2));
+        assertFalse(problem.isValidBST(node5));
+    }
+
+    @Test
+    public void testInsertIntoBST() {
+        TreeNode node1 = new TreeNode(1);
+        TreeNode node3 = new TreeNode(3);
+        TreeNode node2 = new TreeNode(2, node1, node3);
+
+        TreeNode node7 = new TreeNode(7);
+        TreeNode node4 = new TreeNode(4, node2, node7);
+        assertEquals(problem.insertIntoBST(node4, 6).right.left.val, 6);
+    }
+
+    @Test
+    public void deleteNode() {
+        TreeNode node2 = new TreeNode(2);
+        TreeNode node4 = new TreeNode(4);
+        TreeNode node3 = new TreeNode(3, node2, node4);
+
+        TreeNode node7 = new TreeNode(7);
+        TreeNode node6 = new TreeNode(6, null, node7);
+        TreeNode node5 = new TreeNode(5, node3, node6);
+        TreeNode root = problem.deleteNode(node5, 3);
+        assertEquals(root.left.val, 4);
+        assertEquals(root.left.left.val, 2);
+        assertNull(root.left.right);
+    }
+
+    /**
+     * build tree
+     *             4
+     *         2      7
+     *      1   3  6     9
+     *
+     * @return root node
+     */
+    private TreeNode buildCommonTree() {
+        TreeNode node1 = new TreeNode(1);
+        TreeNode node3 = new TreeNode(3);
+        TreeNode node2 = new TreeNode(2, node1, node3);
+
+        TreeNode node6 = new TreeNode(6);
+        TreeNode node9 = new TreeNode(9);
+        TreeNode node7 = new TreeNode(7, node6, node9);
+
+        return new TreeNode(4, node2, node7);
+    }
+
 }
